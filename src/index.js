83dd7c8a250847e8b10e1396185704be13d8d4ab
rindra josia <rrindrajosia@gmail.com>
2021-12-03 21:37:18 +0300

    Organize Project, Create Routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/App';
+import Login from './Components/Auth/Login';
+import Register from './Components/Auth/Register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Root = () => (
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<App />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+    </Routes>
+  </Router>
 );
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
